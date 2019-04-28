466deb93bb9a5910d4a2525d357dacc869c2bb3e
Stephen Sorley <stephen.sorley@gmail.com>
2019-04-27 22:30:09 -0400

    cmake: InstallDeps now adds extra Qt plugins and libs as needed

--- cmake/InstallDeps.cmake
+++ cmake/InstallDeps.cmake
@@ -62,7 +62,7 @@ function(_install_deps_get_dll_from_implib out_dll path)
     string(TOLOWER "${libname}" libname_lower)
     string(TOUPPER "${libname}" libname_upper)
 
-    if (NOT libname)
+    if(NOT libname)
         # Get library name by removing .lib or .dll.a from extension.
         get_filename_component(imp_file "${path}" NAME)
         string(REGEX REPLACE "\.lib$" "" libname "${imp_file}")
@@ -80,23 +80,23 @@ function(_install_deps_get_dll_from_implib out_dll path)
         list(APPEND alt_names ${alt_name})
         string(REGEX REPLACE "[dD]$" "" alt_name "${nolibname}")
         list(APPEND alt_names ${alt_name})
-    endif ()
+    endif()
 
     string(MAKE_C_IDENTIFIER "${libname}" clibname)
 
     # Figure out possible arch names, based on bitness of project.
     set(suffixes)
-    if (CMAKE_SIZEOF_VOID_P EQUAL 8)
+    if(CMAKE_SIZEOF_VOID_P EQUAL 8)
         list(APPEND suffixes
             64 bin64 64/bin bin/64 lib64 64/lib lib/64
             x86_64 x86_64/bin bin/x86_64 x86_64/lib lib/x86_64
         )
-    else ()
+    else()
         list(APPEND suffixes
             32 bin32 32/bin bin/32 lib32 32/lib lib/32
             x86 x86/bin bin/x86 x86/lib lib/x86
         )
-    endif ()
+    endif()
     list(APPEND suffixes bin lib)
 
     set(CMAKE_FIND_LIBRARY_SUFFIXES .dll)
@@ -108,85 +108,159 @@ function(_install_deps_get_dll_from_implib out_dll path)
         PATH_SUFFIXES   ${suffixes}
     )
 
-    if (${clibname}_DLL)
+    if(${clibname}_DLL)
         set(${out_dll} "${${clibname}_DLL}" PARENT_SCOPE)
-    else ()
+    else()
         message(FATAL_ERROR "install_dep_libs() couldn't find DLL for given import lib \"${path}\" (set path with -D${clibname}_DLL=...)")
-    endif ()
+    endif()
 endfunction()
 
 
 # Helper function for _install_deps_internal: convert given list of libs into paths.
 function(_install_deps_get_paths_from_libs lib_dest runtime_dest component out_paths_name out_libs_name)
     set(out_libs)
-    foreach (lib IN LISTS ${out_libs_name})
+    foreach(lib IN LISTS ${out_libs_name})
         # Skip empty list elements, as well as "optimized" and "debug" keywords that might be in a <NAME>_LIBRARIES variable.
-        if ((NOT lib) OR (lib STREQUAL "optimized") OR (lib STREQUAL "debug"))
+        if((NOT lib) OR (lib STREQUAL "optimized") OR (lib STREQUAL "debug"))
             continue()
-        endif ()
+        endif()
 
         if (TARGET "${lib}")
             # If this is an alias target, get the proper name of the target, then add the result back
             # onto the list of libs to process on the next invocation of this function.
             get_target_property(alias ${lib} ALIASED_TARGET)
-            if (alias)
+            if(alias)
                 list(APPEND out_libs "${alias}")
                 continue()
-            endif ()
+            endif()
 
             # If this target isn't a shared, module or interface library target, skip it silently without doing anything.
             get_target_property(type ${lib} TYPE)
-            if (NOT type STREQUAL "SHARED_LIBRARY" AND
-                NOT type STREQUAL "MODULE_LIBRARY" AND
-                NOT type STREQUAL "INTERFACE_LIBRARY" AND
-                NOT type STREQUAL "UNKNOWN_LIBRARY")  #UNKNOWN is a special type that only applies to import libraries.
+            if(NOT type STREQUAL "SHARED_LIBRARY" AND
+               NOT type STREQUAL "MODULE_LIBRARY" AND
+               NOT type STREQUAL "INTERFACE_LIBRARY" AND
+               NOT type STREQUAL "UNKNOWN_LIBRARY")  #UNKNOWN is a special type that only applies to import libraries.
                 continue()
-            endif ()
+            endif()
 
             # If this target isn't imported, install directly if shared or module, then skip regardless of type.
             get_target_property(is_imported ${lib} IMPORTED)
-            if (NOT is_imported)
-                if (type STREQUAL "SHARED_LIBRARY" OR type STREQUAL "MODULE_LIBRARY")
+            if(NOT is_imported)
+                if(type STREQUAL "SHARED_LIBRARY" OR type STREQUAL "MODULE_LIBRARY")
                     install(TARGETS ${lib}
                         LIBRARY DESTINATION "${lib_dest}"
                         RUNTIME DESTINATION "${runtime_dest}"
                         ${component}
                     )
-                endif ()
+                endif()
                 continue()
-            endif ()
+            endif()
 
             # For imported interface libraries, grab the value of interface link libraries, and add the results
             # back onto the list of libs to process on the next invocation of this function.
-            if (type STREQUAL "INTERFACE_LIBRARY")
+            if(type STREQUAL "INTERFACE_LIBRARY")
                 get_target_property(dep_libs ${lib} INTERFACE_LINK_LIBRARIES)
-                if (dep_libs)
+                if(dep_libs)
                     list(APPEND out_libs "${dep_libs}")
-                endif ()
+                endif()
                 continue()
-            endif ()
+            endif()
 
             # For shared/module imported libs, get the imported location (should be DLL, on Windows). Add to list of paths.
             get_target_property(lib_path ${lib} LOCATION)
-            if (WIN32 AND NOT lib_path)
+            if(WIN32 AND NOT lib_path)
                 # If there's no known DLL, use the import lib as the path instead.
                 get_target_property(lib_path ${lib} IMPORTED_IMPLIB)
-            endif ()
-            if (NOT EXISTS "${lib_path}")
+            endif()
+            if(NOT EXISTS "${lib_path}")
                 message(FATAL_ERROR "Target ${lib} given to install_dep_libs() contained bad path ${lib_path}")
-            endif ()
+            endif()
             list(APPEND ${out_paths_name} "${lib_path}")
-        elseif (lib)
+        elseif(lib)
             # Handling for if this lib is a file path.
             if (NOT EXISTS "${lib}")
                 message(FATAL_ERROR "Path ${lib} given to install_dep_libs() was bad")
-            endif ()
+            endif()
             list(APPEND ${out_paths_name} "${lib}")
-        endif ()
-    endforeach ()
+        endif()
+    endforeach()
 
     set(${out_paths_name} "${${out_paths_name}}" PARENT_SCOPE)
     set(${out_libs_name} "${out_libs}" PARENT_SCOPE)
+endfunction()
+
+
+# Helper function for _install_deps_internal: if we're installing Qt runtime libraries, add extra
+# Qt libs and plugins that are required for Qt to run properly, but aren't explicitly listed as
+# dependencies in the imported library targets.
+#
+# ARGN == list of qt import libs provided to application.
+function(_install_deps_get_qt_extra_paths lib_dest runtime_dest component out_paths_name)
+    set(qt_import_libs ${ARGN})
+    if(NOT qt_import_libs)
+        return()
+    endif()
+
+    set(to_add)
+
+    if("Qt5::Core" IN_LIST qt_import_libs)
+        # Get root dir of Qt5 installation.
+        #   Find absolute path to core library.
+        get_target_property(qt5_root Qt5::Core LOCATION)
+        #   Remove library file from path.
+        get_filename_component(qt5_root "${qt5_root}" DIRECTORY)
+        #   Remove last directory ("lib") from path.
+        get_filename_component(qt5_root "${qt5_root}" DIRECTORY)
+
+        # Install platform plugin. Don't add to out_paths, this has to go in a special subdir,
+        # and it's guaranteed not to need symlinks.
+        set(plugin)
+        if(WIN32)
+            set(plugin "qwindows.dll")
+        elseif(NOT APPLE) # Linux/X11
+            set(plugin "libqxcb.so")
+        endif()
+
+        if(plugin)
+            set(plugin "${qt5_root}/plugins/platforms/${plugin}")
+            if(EXISTS "${plugin}")
+                install(FILES "${plugin}"
+                    DESTINATION "${runtime_dest}/platforms"
+                    ${component}
+                )
+            endif()
+        endif()
+
+        # Install any extra libs needed by Qt5::Core.
+        if(NOT WIN32 AND NOT APPLE) # Linux
+            # Linux XCB platform plugin requires a few additional libraries that aren't deps
+            # of other modules.
+            list(APPEND to_add
+                libQt5DBus.so
+                libQt5XcbQpa.so
+            )
+
+            # Qt can't rely on OS libs for internationalization on Linux, because they're not
+            # standardized enough. So, Qt is built against one specific version of ICU that must
+            # be bundled alongside its libraries. It has to be the exact version bundled with Qt.
+            list(APPEND to_add
+                libicui18n.so
+                libicuuc.so
+                libicudata.so
+            )
+        endif()
+    endif()
+
+    # Add prefix to each lib name we want to add.
+    list(TRANSFORM to_add PREPEND "${qt5_root}/lib/")
+    foreach(lib IN LISTS to_add)
+        if(EXISTS ${lib})
+            list(APPEND ${out_paths_name} ${lib})
+        endif()
+    endforeach()
+
+    # Push changes to out_paths up to caller's scope.
+    set(${out_paths_name} "${${out_paths_name}}" PARENT_SCOPE)
 endfunction()
 
 
@@ -195,9 +269,9 @@ function(_install_deps_read_soname outvarname path)
     # Set output variable to empty string - this is what will be returned on an error.
     set(${outvarname} "" PARENT_SCOPE)
 
-    if (NOT READELF) # If this system doesn't provide the readelf command.
+    if(NOT READELF) # If this system doesn't provide the readelf command.
         return()
-    endif ()
+    endif()
 
     # Read the ELF header from the file.
     execute_process(COMMAND "${READELF}" -d ${path}
@@ -206,22 +280,22 @@ function(_install_deps_read_soname outvarname path)
         ERROR_QUIET
         OUTPUT_STRIP_TRAILING_WHITESPACE
     )
-    if (NOT res EQUAL 0) # If the readelf command returned an error status code.
+    if(NOT res EQUAL 0) # If the readelf command returned an error status code.
         return()
-    endif ()
+    endif()
 
     # Parse the SONAME out of the header.
-    if (READELF MATCHES "readelf")
+    if(READELF MATCHES "readelf")
         # Linux (readelf) format:   0x000000000000000e (SONAME) Library soname: [libssl.so.1.0.0]
-        if (NOT header MATCHES "\\(SONAME\\)[^\n]+\\[([^\n]+)\\]")
+        if(NOT header MATCHES "\\(SONAME\\)[^\n]+\\[([^\n]+)\\]")
             return()
-        endif ()
-    else ()
+        endif()
+    else()
         # Solaris (elfdump) format: [8] SONAME 0x49c1 libssl.so.1.0.0
-        if (NOT header MATCHES "\\[[0-9]+\\][ \t]+SONAME[ \t]+[x0-9a-fA-F]+[ \t]+([^\n]+)")
+        if(NOT header MATCHES "\\[[0-9]+\\][ \t]+SONAME[ \t]+[x0-9a-fA-F]+[ \t]+([^\n]+)")
             return()
-        endif ()
-    endif ()
+        endif()
+    endif()
 
     string(STRIP "${CMAKE_MATCH_1}" soname)
     set(${outvarname} "${soname}" PARENT_SCOPE)
@@ -231,9 +305,9 @@ endfunction()
 # Helper function for install_deps.
 # _install_deps_internal([lib dest] [runtime dest] [flag to turn file copy on/off] [flag to turn file install on/off] [... lib files or import targets ...]
 function(_install_deps_internal lib_dest runtime_dest component do_copy do_install)
-    if ((NOT do_copy) AND (NOT do_install))
+    if((NOT do_copy) AND (NOT do_install))
         return()
-    endif ()
+    endif()
 
     # Handle default destination for copied DLL's.
     set(copy_dest "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}")
@@ -241,64 +315,85 @@ function(_install_deps_internal lib_dest runtime_dest component do_copy do_insta
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     # Construct list of library paths.
     set(lib_paths)
-    if (NOT lib_dest)
+    if(NOT lib_dest)
         set(lib_dest lib)
-    endif ()
-    if (NOT runtime_dest)
+    endif()
+    if(NOT runtime_dest)
         set(runtime_dest bin)
-    endif ()
+    endif()
 
+    # If any Qt import libraries were passed in, save them in a separate list.
+    set(qt_import_libs)
+    foreach(lib IN LISTS libs)
+        if(lib MATCHES "Qt[0-9]+::")
+            list(APPEND qt_import_libs "${lib}")
+        endif()
+    endforeach()
+
+    # Convert list of libraries into a list of library paths. Will resolve any alias libraries,
+    # import libs, and import lib dependencies into full paths. Will also filter out unwanted
+    # stuff like static library targets.
     set(libs ${ARGN})
-    while (libs)
-        _install_deps_get_paths_from_libs("${lib_dest}" "${runtime_dest}" "${component}" lib_paths libs)
-    endwhile ()
+    while(libs)
+        _install_deps_get_paths_from_libs("${lib_dest}" "${runtime_dest}" "${component}"
+            lib_paths
+            libs
+        )
+    endwhile()
+
+    # If we're installing any Qt libs, add any extra plugins and libraries that are required for
+    # them to work properly.
+    _install_deps_get_qt_extra_paths("${lib_dest}" "${runtime_dest}" "${component}"
+        lib_paths
+        ${qt_import_libs}
+    )
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     # Process each library path, install to appropriate location.
     set(sonames)
-    foreach (path IN LISTS lib_paths)
+    foreach(path IN LISTS lib_paths)
         # If this is an empty list element, skip it.
-        if (NOT path)
+        if(NOT path)
             continue()
-        endif ()
+        endif()
 
         # If on Windows, try to replace import libraries with DLL's. Throws fatal error if it can't do it.
-        if (WIN32 AND (${path} MATCHES "\.lib$" OR ${path} MATCHES "\.a$"))
+        if(WIN32 AND (${path} MATCHES "\.lib$" OR ${path} MATCHES "\.a$"))
            _install_deps_get_dll_from_implib(path "${path}")
-        endif ()
+        endif()
 
         # Resolve any symlinks in path to get actual physical name. If relative, evaluate relative to current binary dir.
         get_filename_component(path "${path}" REALPATH BASE_DIR "${CMAKE_CURRENT_BINARY_DIR}")
 
         # Figure out the destination (DLL's and manifest files go to runtime_dest, everything else goes to lib_dest).
-        if (path MATCHES "\.[dD][lL][lL]$" OR path MATCHES "\.[mM][aA][nN][iI][fF][eE][sS][tT]$")
+        if(path MATCHES "\.[dD][lL][lL]$" OR path MATCHES "\.[mM][aA][nN][iI][fF][eE][sS][tT]$")
             set(dest "${runtime_dest}")
-            if (path MATCHES "\.[dD][lL][lL]$")
+            if(path MATCHES "\.[dD][lL][lL]$")
                 set(type PROGRAMS) # install DLL's as executable
-            else ()
+            else()
                 set(type FILES) # install manifests as a normal non-executable file
-            endif ()
+            endif()
 
             # If requested by caller, copy the DLL's to the build dir in addition to installing them.
             # If the file with the same name and timestamp already exists at the destination, nothing will be copied.
-            if (do_copy)
-                if (CMAKE_CONFIGURATION_TYPES)
+            if(do_copy)
+                if(CMAKE_CONFIGURATION_TYPES)
                     foreach(conf ${CMAKE_CONFIGURATION_TYPES})
                         file(MAKE_DIRECTORY "${copy_dest}/${conf}")
                         file(COPY "${path}" DESTINATION "${copy_dest}/${conf}")
                     endforeach()
-                else ()
+                else()
                     file(COPY "${path}" DESTINATION "${copy_dest}")
-                endif ()
-            endif ()
-        else ()
+                endif()
+            endif()
+        else()
             set(dest "${lib_dest}")
             set(type FILES)
-        endif ()
+        endif()
 
-        if (NOT do_install)
+        if(NOT do_install)
             continue()
-        endif ()
+        endif()
 
         # Install the file.
         #message(STATUS "will install ${path} to ${dest}")
@@ -306,9 +401,9 @@ function(_install_deps_internal lib_dest runtime_dest component do_copy do_insta
 
         # If the library has a soname that's different than the actual name of the file on disk, add a symlink.
         _install_deps_read_soname(soname "${path}")
-        if (soname)
+        if(soname)
             get_filename_component(libname "${path}" NAME)
-            if (NOT soname STREQUAL libname)
+            if(NOT soname STREQUAL libname)
                 # Generate a relative-path symlink in the build dir (doesn't have to be valid).
                 set(tmpdir "${CMAKE_CURRENT_BINARY_DIR}/dep-lib-links")
                 file(MAKE_DIRECTORY "${tmpdir}")
@@ -321,41 +416,41 @@ function(_install_deps_internal lib_dest runtime_dest component do_copy do_insta
                     OUTPUT_QUIET
                 )
 
-                if (NOT res EQUAL 0)
+                if(NOT res EQUAL 0)
                     message(AUTHOR_WARNING "install_deplib: failed to create install symlink for \"${libname}\"")
                     continue()
-                endif ()
+                endif()
 
                 # Install the symlink to the same directory as the library.
                 #message(STATUS "will install ${tmpdir}/${soname} to ${dest}")
                 install(${type} "${tmpdir}/${soname}" DESTINATION "${dest}" ${component})
-            endif ()
-        endif ()
-    endforeach ()
+            endif()
+        endif()
+    endforeach()
 endfunction()
 
 
 # install_deps([lib dest] [runtime dest] [... lib files or import targets ...]
 function(install_deps lib_dest runtime_dest)
     # Handle default values for variables that control DLL copying.
-    if (NOT DEFINED INSTALL_DEPS_COPY_DLL)
+    if(NOT DEFINED INSTALL_DEPS_COPY_DLL)
         set(INSTALL_DEPS_COPY_DLL TRUE)
-    endif ()
+    endif()
 
     set(libs ${ARGN})
-    if (NOT libs)
+    if(NOT libs)
         return()
-    endif ()
+    endif()
 
     # See if the user passed an optional "COMPONENT [component name]" to the install command.
     # If they did, remove those entries from the 'libs' list and add them to the 'component' list.
     set(component)
     list(FIND libs COMPONENT idx)
-    if (idx GREATER -1)
+    if(idx GREATER -1)
         math(EXPR idx_next "${idx} + 1")
         list(GET libs ${idx} ${idx_next} component)
         list(REMOVE_AT libs ${idx} ${idx_next})
-    endif ()
+    endif()
 
     # Call internal helper
     _install_deps_internal("${lib_dest}" "${runtime_dest}" "${component}" ${INSTALL_DEPS_COPY_DLL} TRUE ${libs})
